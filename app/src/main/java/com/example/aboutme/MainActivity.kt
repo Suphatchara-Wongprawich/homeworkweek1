0b092c52fc03dcc0c0fa5e9a970d7a2e74ef540a
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-07-20 21:33:03 +0700

    Step 3: Hide the keyboard (2)

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -29,8 +29,8 @@ private fun addNickname(view: View) {
     nicknameTextView.visibility = View.VISIBLE
 
     // Hide the keyboard.
-    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(view.windowToken, 0)
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 
