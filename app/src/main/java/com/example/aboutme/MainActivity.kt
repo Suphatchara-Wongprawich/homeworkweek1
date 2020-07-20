19c8b6651d497cbe8cdf83d262303511a3792c8f
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-07-20 21:25:22 +0700

    Step 3: Hide the keyboard

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
     }
 }
 private fun addNickname(view: View) {
@@ -24,8 +29,10 @@ private fun addNickname(view: View) {
     editText.visibility = View.GONE
     nicknameTextView.visibility = View.VISIBLE
 
-    findViewById<Button>(R.id.done_button).setOnClickListener {
-        addNickname(it)
+    // Hide the keyboard.
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 
 
